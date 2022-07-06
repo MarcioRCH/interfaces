398f4343b206cee0dc7c703fdeb20e6a217a8663
MarcioRCH <marciorchsilva@gmail.com>
2022-07-06 17:28:29 -0300

    Initial implementation

---
+++ src2/model/entities/Contract.java
@@ -0,0 +1,13 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Contract {
+	
+	private Integer number;
+	private Date date;
+	private Double totalValue;
+	
+	public Contract() {
+	}
+}
